945dbbefe083676fd3ec78c7e6e524cec8933576
zakkiiy <131367248+zakkiiy@users.noreply.github.com>
2023-11-20 17:58:29 +0900

    devise関連の設定 usersのMVCの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords',
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    unlocks: 'users/unlocks'
+  }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
